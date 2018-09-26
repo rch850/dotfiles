adec1d15692ea58e976736eb9846bdb4238dbda3
rch850 <rich850@gmail.com>
2018-09-27 00:13:52 +0900

    Add fish nvm function

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,3 +3,8 @@ if not test -f ~/.config/fish/functions/fisher.fish
   curl -sLo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisher
   fisher
 end
+
+function nvm
+  bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
+end
+
---
+++ README.md
@@ -0,0 +1,8 @@
+## nvm
+
+```
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+```
+
+See https://github.com/creationix/nvm
+
